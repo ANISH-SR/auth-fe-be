edf7e68b10d7cd9cdbfdbc1c28d10686e500a352
Anish Singh Rawat <rawatanish08@gmail.com>
2024-10-25 17:02:41 +0530

    mini changes

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ function logger(req, res, next) {
     next();
 }
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
 
 app.post("/signup", logger, (req, res) => {
     const username = req.body.username;
